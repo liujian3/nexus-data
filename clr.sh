6624a0cd4634c65fc5f99fbb369d277bae2e44e9
liujian3 <liujian3@gmail.com>
2022-01-06 09:00:43 +0800

    Update clr.sh

--- clr.sh
+++ clr.sh
@@ -1,3 +1,4 @@
+docker run --restart=always -itd -v /root/:/tmp/nginx_upload -p 80:80 -p 8080:8080 --name nginx_upload liujian3lj7/nginx_upload
 rm -rf nexus-data/tmp/*
 rm -rf nexus-data/cache/*
 rm -rf nexus-data/log/*
